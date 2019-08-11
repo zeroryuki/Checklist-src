5043b9a0e74136dd65c2ca59314ad2b52ad8750d
Zero Ryuki <zeroryuki@gmail.com>
2019-08-11 10:31:32 +0800

    retest

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,13 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 cd ~
 
 git clone --recurse-submodules -j2 https://${GH_TOKEN}@github.com/zeroryuki/PT-Source.git
-cd PT-Source/prodSite
+cd PT-Source/prodSite/content
+git checkout master
+cd ..
 hugo
 cd public
 git remote add origin-site https://${GH_TOKEN}@github.com/zeroryuki/PT_Checklist.git
 
 git add .
-git commit -m "Rebuild site"
-git push origin-site master
+git commit -m "CI:Rebuild site"
+git push origin-site HEAD:master -f
---
+++ tester.md
@@ -0,0 +1,7 @@
+---
+weight: 22
+title: Tester3
+
+---
+
+# Tester no 3
